763a482e75c7b6f52093a9b72a96f7e4278db09b
Nephrus <3471753547@qq.com>
2026-08-26 18:56:12 +0800

    Implement RUN_ONCE for one-time sunset data check
    Add support for RUN_ONCE environment variable to execute sunset check once.

--- main.go
+++ main.go
@@ -5,18 +5,27 @@ import (
 	"flame_clouds/flags"
 	"flame_clouds/global"
 	"flame_clouds/service/cron_service"
+	"flame_clouds/service/hsy_service"
+	"os"
 )
 
 func main() {
 	// 读取配置文件
 	global.Config = core.ReadConfig()
+
 	// 日志
 	core.InitLogger()
 
 	// 命令行参数绑定
 	flags.Run()
 
-	// 定时任务
+	// 如果设置 RUN_ONCE=true，则只执行一次晚霞检查
+	if os.Getenv("RUN_ONCE") == "true" {
+		hsy_service.GetCitySunsetData(global.Config.Monitor.Evening)
+		return
+	}
+
+	// 正常模式：启动定时任务
 	cron_service.CronService()
 
 	select {}
